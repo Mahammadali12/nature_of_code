e3a015f5d97b315a9181cc46a54cbdaf6ca4e358
Mahammadali12 <zamanli.mehemmedeli@gmail.com>
2025-10-20 13:58:25 +0400

    3D animated terrain

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-  createCanvas(600, 400, WEBGL);
+  createCanvas(600, 600, WEBGL);
   noFill();
   stroke(255);
 }
@@ -11,19 +11,23 @@ let yOff = start;
 function draw() {
   background(51);
 
-  rotateX(PI/3);                  // tilt the “floor”
-  rotateZ(frameCount * 0.01);     // spin around vertical axis maybe
+  
+  rotateX(PI/3);                   // tilt the “floor”
+  // rotateY(PI/3);                   // tilt the “floor”
+  // rotateZ( frameCount * 0.01);     // spin around vertical axis maybe
 
-  let rows = floor((height * 2) / scaleFactor);  // approximate because WEBGL origin is center
-  let cols = floor((width * 2) / scaleFactor);
+  let rows = floor((height) / scaleFactor);  // approximate because WEBGL origin is center
+  let cols = floor((width) / scaleFactor);
 
   yOff = start
   for (let y = 0; y < rows - 1; y++) {
     xOff = 0
     beginShape(TRIANGLE_STRIP);
     for (let x = 0; x < cols; x++) {
-      vertex(x * scaleFactor - width/2,   y * scaleFactor - height/2,map(noise(xOff,yOff),0,1,-50,50));
-      vertex(x * scaleFactor - width/2,  (y + 1) * scaleFactor - height/2, map(noise(xOff,yOff),0,1,-50,50));
+      vertex(x * scaleFactor - width/2,   y * scaleFactor - height/2);
+      vertex(x * scaleFactor - width/2,  (y + 1) * scaleFactor - height/2);
+      // vertex(x * scaleFactor - width/2,   y * scaleFactor - height/2,map(noise(xOff,yOff),0,1,-50,50));
+      // vertex(x * scaleFactor - width/2,  (y + 1) * scaleFactor - height/2, map(noise(xOff,yOff),0,1,-50,-50));
       xOff += 0.1
     }
     yOff += 0.1
