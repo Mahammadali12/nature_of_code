0e20329d6e0332f0b991ed2fca47085cf13440bc
Mahammadali12 <zamanli.mehemmedeli@gmail.com>
2025-10-24 13:25:59 +0400

    Refactor terrain setup and rendering logic for improved clarity and performance

--- sketch.js
+++ sketch.js
@@ -1,10 +1,18 @@
 let land;
 let theta = 0;
+let rows, columns;
+let height, width;
+let scl = 20;
 
 
 function setup() {
   createCanvas(640, 400, WEBGL);
-  land = new terrain(20, 400, 400);
+  height = 400;
+  width = 400;
+
+  land = new terrain(scl, height, width);
+  rows = floor(height / scl);
+  columns = floor(width / scl);
   // noFill();
 }
 
@@ -14,46 +22,11 @@ function draw() {
   push();
   translate(-width/2, -height/2, -300);
   rotateX(PI / 3);
-  rotateZ(theta);
+  // rotateZ(theta);
   land.render();
   pop();
-  theta += 0.025;
-
-
+  theta += 0.0025;
 }  
-
-// let scaleFactor = 20;
-// let start = 0
-// let xOff = 0;
-// let yOff = start;
-// function draw() {
-//   background(51);
-
-  
-//   rotateX(PI/3);                   // tilt the “floor”
-//   rotateZ( frameCount * 0.005);     // spin around vertical axis maybe
-
-//   let rows = floor((height) / scaleFactor);  // approximate because WEBGL origin is center
-//   let cols = floor((width) / scaleFactor);
-
-//   yOff = start
-//   for (let y = 0; y < rows - 1; y++) {
-//     xOff = 0
-//     beginShape(TRIANGLE_STRIP);
-//     for (let x = 0; x < cols; x++) {
-//       // vertex(x * scaleFactor - width/2,   y * scaleFactor - height/2);
-//       // vertex(x * scaleFactor - width/2,  (y + 1) * scaleFactor - height/2);
-//       vertex(x * scaleFactor - width/2,   y * scaleFactor - height/2,map(noise(xOff,yOff),0,1,-50,50));
-//       vertex(x * scaleFactor - width/2,  (y + 1) * scaleFactor - height/2, map(noise(xOff,yOff),0,1,-50,-50));
-//       xOff += 0.1
-//     }
-//     yOff += 0.1
-//     endShape();
-//   }
-//   start += 0.01
-// }
-
-
 
 class terrain {
 
@@ -76,10 +49,12 @@ class terrain {
   }
 
   calculate(){
+    // console.log(this.z.length)
+    // console.log(this.z[0].length)
     this.yOff = 0;
-    for (let y = 0; y < this.rows; y++) {
+    for (let y = 0; y < rows; y++) {
       this.xOff = 0;
-      for (let x = 0; x < this.columns; x++) {
+      for (let x = 0; x < columns; x++) {
         this.z[x][y] = map(noise(this.xOff, this.yOff, this.zOff), 0, 1, -100, 100);
         // this.x = x * this.scl - width / 2;
         // this.y = y * this.scl - height / 2;
@@ -92,16 +67,25 @@ class terrain {
 
   render(){
 
-    for (let  x = 0;  x < this.z.length-1;  x++) {
+    for (let  x = 0;  x < columns;  x++) {
       beginShape(QUAD_STRIP);
-      for (let y = 0; y < this.z[x].length; y++) {
+      for (let y = 0; y < rows; y++) {
+        // console.log(x,y);
         stroke(0);
-        let xPos = x * this.scl - (this.rows) / 2;
-        let yPos = y * this.scl - (this.columns) / 2;
+        let xPos = x * this.scl;
+        let yPos = y * this.scl;
         fill(map(this.z[x][y],-100,100,0,255), 255);
 
+        // vertex(xPos, yPos, 0);
+        // vertex(xPos + this.scl,yPos, 0);
         vertex(xPos, yPos, this.z[x][y]);
-        vertex(xPos + this.scl,yPos, this.z[x + 1][y]);
+        if (x+1 === columns) {
+          vertex(xPos + this.scl,yPos, this.z[x][y]);
+          
+        }else{
+          vertex(xPos + this.scl,yPos, this.z[x + 1][y]);
+        }
+
       }
       endShape();
     }
